58426763741d1f1bee7e2cf748713274a497f3e6
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-23 13:29:37 +0700

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Meme } from "./Meme/Meme";
+import { Switch, Route } from "react-router-dom";
+import { MemeGenerated } from "./MemeGenerated/MemeGenerated";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Meme Generator</h1>
+      <Switch>
+        <Route exact path="/">
+          <Meme />
+        </Route>
+        <Route path="/generated">
+          <MemeGenerated />
+        </Route>
+      </Switch>
     </div>
   );
-}
-
-export default App;
+};
